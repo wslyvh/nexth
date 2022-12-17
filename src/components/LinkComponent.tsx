610849eb4baef296e7a3a55439daff92edad9716
wslyvh <wslyvh@users.noreply.github.com>
2022-12-17 13:30:38 -0500

    lint fix

--- src/components/LinkComponent.tsx
+++ src/components/LinkComponent.tsx
@@ -13,26 +13,18 @@ interface Props {
 export function LinkComponent(props: Props) {
   const className = props.className ?? ''
   const isExternal = props.href.match(/^([a-z0-9]*:|.{0})\/\/.*$/) || props.isExternal
+  const color = useColorModeValue(`${THEME_COLOR_SCHEME}.600`, `${THEME_COLOR_SCHEME}.400`)
 
   if (isExternal) {
     return (
-      <Link
-        className={className}
-        _hover={{ color: useColorModeValue(`${THEME_COLOR_SCHEME}.600`, `${THEME_COLOR_SCHEME}.400`) }}
-        href={props.href}
-        target="_blank"
-        rel="noopener noreferrer">
+      <Link className={className} _hover={{ color: color }} href={props.href} target="_blank" rel="noopener noreferrer">
         {props.children}
       </Link>
     )
   }
 
   return (
-    <Link
-      as={NextLink}
-      className={className}
-      _hover={{ color: useColorModeValue(`${THEME_COLOR_SCHEME}.600`, `${THEME_COLOR_SCHEME}.400`) }}
-      href={props.href}>
+    <Link as={NextLink} className={className} _hover={{ color: color }} href={props.href}>
       {props.children}
     </Link>
   )
