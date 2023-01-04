d68024ddb84911666da5a2fbecad4ae753fa4c6a
wslyvh <wslyvh@users.noreply.github.com>
2023-01-04 16:13:09 +0100

    sign & verify messages

---
+++ src/pages/examples/sign.tsx
@@ -0,0 +1,81 @@
+import * as React from 'react'
+import { useAccount, useSignMessage } from 'wagmi'
+import { Button, FormControl, FormLabel, Heading, Textarea } from '@chakra-ui/react'
+import { useState } from 'react'
+import { verifyMessage } from 'ethers/lib/utils'
+import { SignMessageArgs } from '@wagmi/core'
+
+function SignMessage() {
+  let [message, setMessage] = useState('')
+  let [address, setAddress] = useState('')
+  const signMessage = useSignMessage({
+    message,
+    onSuccess: verify,
+  })
+
+  function submit() {
+    signMessage.signMessage({ message })
+  }
+
+  function verify(data: string, variables: SignMessageArgs) {
+    const address = verifyMessage(variables.message, data)
+    setAddress(address)
+  }
+
+  return (
+    <div>
+      <Heading as="h3" fontSize="xl" my={4}>
+        Sign Message
+      </Heading>
+
+      <FormControl>
+        <FormLabel>Message</FormLabel>
+        <Textarea value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter a message to sign" />
+
+        <Button mt={4} type="submit" onClick={submit}>
+          Sign
+        </Button>
+
+        {signMessage.data && (
+          <div>
+            <Heading as="h3" fontSize="xl" my={4}>
+              Signature
+            </Heading>
+            <p>{signMessage.data}</p>
+          </div>
+        )}
+
+        {address && (
+          <div>
+            <Heading as="h3" fontSize="xl" my={4}>
+              Signed by
+            </Heading>
+            <p>{address}</p>
+          </div>
+        )}
+      </FormControl>
+    </div>
+  )
+}
+
+export default function SignExample() {
+  const { isConnected } = useAccount()
+
+  if (isConnected) {
+    return (
+      <div>
+        <Heading as="h2" fontSize="2xl" my={4}>
+          Sign and verify messages
+        </Heading>
+        <p>
+          Private keys can be used to sign any kind of messages. This is useful for verifying that a message was sent by a specific account. This is
+          also how transactions are (signed and) send to the network.
+        </p>
+
+        <SignMessage />
+      </div>
+    )
+  }
+
+  return <div>Connect your wallet first to sign a message.</div>
+}
