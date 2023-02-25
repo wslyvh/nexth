3447a51b2e90bd74ae11f7b70596dfc82372b273
wslyvh <wslyvh@users.noreply.github.com>
2023-02-25 17:02:10 +0100

    learn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,47 @@
 import { Text } from '@chakra-ui/react'
+import { CardList } from 'components/layout/CardList'
 import { Head } from 'components/layout/Head'
 import { HeadingComponent } from 'components/layout/HeadingComponent'
-import { LinkComponent } from 'components/layout/LinkComponent'
+import { SITE_NAME, SITE_DESCRIPTION } from 'utils/config'
+
+import FundamentalsIcon from 'assets/icons/blockchain.png'
+import WalletIcon from 'assets/icons/wallet.png'
+import EthereumIcon from 'assets/icons/ether.png'
+import ContractsIcon from 'assets/icons/smart-contracts.png'
+import SolidityIcon from 'assets/icons/code.png'
+
+export const CertificationItems = [
+  {
+    title: 'Blockchain Fundamentals',
+    description: 'Learn about the fundamentals of blockchain, how it works and why it matters.',
+    image: FundamentalsIcon.src,
+    url: '/',
+  },
+  {
+    title: 'Wallets',
+    description: 'Learn about wallets, publick-key cryptography, and how to use them to interact with the blockchain.',
+    image: WalletIcon.src,
+    url: '/',
+  },
+  {
+    title: 'Ethereum 101',
+    description: 'Learn about the Ethereum blockchain protocol, how it works and how to build on it.',
+    image: EthereumIcon.src,
+    url: '/',
+  },
+  {
+    title: 'Smart Contracts',
+    description: 'Smart contracts are a fundamental part of the Ethereum ecosystem. Learn more about them here.',
+    image: ContractsIcon.src,
+    url: '/',
+  },
+  {
+    title: 'Solidity',
+    description: 'Learn about the Solidity programming language, how to write smart contracts, and how to deploy them.',
+    image: SolidityIcon.src,
+    url: '/',
+  },
+]
 
 export default function Home() {
   return (
@@ -9,11 +49,10 @@ export default function Home() {
       <Head />
 
       <main>
-        <HeadingComponent as="h2">Next.js + Ethereum starter kit</HeadingComponent>
-        <Text>Quickly ship Web3 Apps ⚡</Text>
-        <Text py={4}>
-          <LinkComponent href="examples">View examples</LinkComponent> to bootstrap development.
-        </Text>
+        <HeadingComponent as="h2">{SITE_NAME}</HeadingComponent>
+        <Text>{SITE_DESCRIPTION}</Text>
+
+        <CardList title="Certifications" items={CertificationItems} />
       </main>
     </>
   )
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,9 +1,9 @@
 import { ThemingProps } from '@chakra-ui/react'
 import { mainnet, goerli, sepolia, polygon, optimism, arbitrum } from '@wagmi/chains'
 
-export const SITE_NAME = 'Nexth'
-export const SITE_DESCRIPTION = 'Next.js + Ethereum starter kit'
-export const SITE_URL = 'https://nexth.vercel.app'
+export const SITE_NAME = 'useWeb3 Learn'
+export const SITE_DESCRIPTION = 'An open education platform to attest your knowledge and claim your ZK certifications.'
+export const SITE_URL = 'https://useweb3-learn.vercel.app/'
 
 export const THEME_INITIAL_COLOR = 'system'
 export const THEME_COLOR_SCHEME: ThemingProps['colorScheme'] = 'gray'
@@ -11,8 +11,8 @@ export const THEME_CONFIG = {
   initialColorMode: THEME_INITIAL_COLOR,
 }
 
-export const SOCIAL_TWITTER = 'wslyvh'
-export const SOCIAL_GITHUB = 'wslyvh/nexth'
+export const SOCIAL_TWITTER = 'useWeb3'
+export const SOCIAL_GITHUB = 'wslyvh/useWeb3'
 
 export const ETH_CHAINS = [mainnet, goerli, sepolia, polygon, optimism, arbitrum]
 
