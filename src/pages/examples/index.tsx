5d0f6256e6d2f50a2867d607cca3586c81e33c0b
wslyvh <wslyvh@users.noreply.github.com>
2023-02-25 12:13:49 +0100

    add examples

--- src/pages/examples/index.tsx
+++ src/pages/examples/index.tsx
@@ -6,6 +6,9 @@ import { Code, Text } from '@chakra-ui/react'
 import SignIcon from '../../assets/icons/fingerprint.png'
 import AuthIcon from '../../assets/icons/auth.png'
 import PassportIcon from '../../assets/icons/passport.png'
+import EtherIcon from '../../assets/icons/ethereum.png'
+import TokenIcon from '../../assets/icons/token.png'
+import NFTIcon from '../../assets/icons/nft.png'
 
 export const ExampleItems = [
   {
@@ -26,6 +29,24 @@ export const ExampleItems = [
       'Gitcoin Passport is an identity protocol that proves your trustworthiness without needing to collect personally identifiable information.',
     image: PassportIcon.src,
     url: '/examples/passport',
+  },
+  {
+    title: 'Send Ether',
+    description: 'Sending Ether to another address is the most basic, common transaction that you can do.',
+    image: EtherIcon.src,
+    url: '/examples/send-ether',
+  },
+  {
+    title: 'Send ERC20 Token',
+    description: 'ERC20 introduces a standard interface for fungible tokens. Use this example to send any ERC20 to another address.',
+    image: TokenIcon.src,
+    url: '/examples/send-erc20',
+  },
+  {
+    title: 'Mint NFT',
+    description: 'A Non-Fungible Token (NFT) is used to identify something or someone in a unique way. Use this ERC721 example to mint your own NFT.',
+    image: NFTIcon.src,
+    url: '/examples/mint-nft',
   },
 ]
 
