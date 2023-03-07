947d99e03806e28c1f318ca7f661f59ad7ee24ac
wslyvh <wslyvh@users.noreply.github.com>
2023-03-07 10:54:23 +0100

    update deploy configs

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,17 +1,29 @@
 import { HardhatUserConfig } from 'hardhat/config'
+import { join } from 'path'
+import dotenv from 'dotenv'
 import '@nomicfoundation/hardhat-toolbox'
+
+dotenv.config({ path: join(process.cwd(), '../.env') })
+
+const deployerKey = process.env.DEPLOYER_KEY
+if (!deployerKey) {
+  console.warn('DEPLOYER_KEY not found in .env file. Running with default config')
+}
 
 const config: HardhatUserConfig = {
   solidity: '0.8.18',
   defaultNetwork: 'hardhat',
   networks: {
-    hardhat: {},
+    hardhat: {
+      chainId: 31337,
+    },
     localhost: {
+      chainId: 31337,
       url: 'http://127.0.0.1:8545',
     },
     sepolia: {
-      url: 'https://rpc.sepolia.dev',
-      accounts: [process.env.DEPLOYER_KEY ?? '0xFFFFFFFFFFFFFFFFFFFFFFFFFFF0000000000000000000000000000000000000'],
+      url: 'https://rpc.sepolia.org/',
+      accounts: [process.env.DEPLOYER_KEY as string],
     },
   },
 }
