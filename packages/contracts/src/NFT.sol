80014b005c5a7fc1eccf1b9de188e3a2f57170f7
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2023-10-11 19:02:19 -0300

    chore: bump oz contracts

--- packages/contracts/src/NFT.sol
+++ packages/contracts/src/NFT.sol
@@ -1,20 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract NexthFT is ERC721, Ownable {
-    using Counters for Counters.Counter;
+  uint256 private _tokenIdCounter;
 
-    Counters.Counter private _tokenIdCounter;
+  constructor() ERC721('NexthFT', 'NFT') Ownable(msg.sender) {}
 
-    constructor() ERC721("NexthFT", "NFT") { }
-
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-    }
+  function safeMint(address to) public onlyOwner {
+    uint256 tokenId = _tokenIdCounter;
+    _tokenIdCounter += 1;
+    _safeMint(to, tokenId);
+  }
 }
--- packages/contracts/test/Message.t.sol
+++ packages/contracts/test/Message.t.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.9.0;
 
-import 'forge-std/Test.sol';
+import {Test} from 'forge-std/Test.sol';
+import {Message} from '../src/Message.sol';
 
 //import "forge-std/console.sol";
 
-contract Message is Test {
+contract MessageTest is Test {
   Message public message;
 
   function setUp() public {
