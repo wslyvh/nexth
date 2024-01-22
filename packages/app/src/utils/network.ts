e4919914df9335fe4953364a8a35df930fd3b890
Maxime <maxime.pizzolitto.82@gmail.com>
2024-01-22 21:37:21 +0100

    fix : add network hardhat

--- packages/app/src/utils/network.ts
+++ packages/app/src/utils/network.ts
@@ -1,13 +1,10 @@
 import { mainnet, sepolia, polygon, optimism, arbitrum, Chain, hardhat } from 'viem/chains'
 
-export const ETH_CHAINS = [
-  mainnet,
-  sepolia,
-  polygon,
-  optimism,
-  arbitrum,
-  process.env.NODE_ENV != 'production' ? hardhat : undefined,
-] as [Chain, ...Chain[]]
+let chains = [mainnet, sepolia, polygon, optimism, arbitrum] as [Chain, ...Chain[]]
+
+if (process.env.NODE_ENV !== 'production') chains.push(hardhat)
+
+export const ETH_CHAINS = chains
 
 export function GetNetworkColor(chain?: string) {
   if (chain === 'homestead') return 'green'
